3773e84a57090f3bacc7d7fcb8134d936399884e
yamato-creator <dydydydydydy5656@gmail.com>
2021-10-21 13:52:54 +0900

    search

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app>
+    <my-header />
+    <nuxt />
+  </v-app>
+</template>
+
+<script>
+import myHeader from '~/components/shared/myHeader'
+
+export default {
+  components: {
+    myHeader
+  }
+}
+</script>
